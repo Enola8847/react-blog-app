ebef48a1f524600e01efface4c5c1f9341834d50
Enola8847 <ice9393th@gmail.com>
2024-09-14 12:06:57 +0700

    blogs เสร็จแล้ว

--- src/page/Blog.js
+++ src/page/Blog.js
@@ -1,11 +1,27 @@
 import { Link } from "react-router-dom";
 import blogs from "../data/blogs";
 import "../page/Blog.css";
+import { useState, useEffect } from "react";
 export default function Blogs() {
+  const [search, SetSearch] = useState("");
+  const [filterblogs, SetFilterblogs] = useState([]);
+  useEffect(() => {
+    const result = blogs.filter((item) => item.title.includes(search));
+    SetFilterblogs(result);
+  }, [search]);
   return (
     <div className="blogs-contanier">
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="ค้นหาบทความ"
+          value={search}
+          onChange={(e) => SetSearch(e.target.value)}
+        ></input>
+      </div>
       <article>
-        {blogs.map((item) => {
+        {filterblogs.map((item) => {
           return (
             <Link to={`/Blogs/${item.id}`} key={item.id}>
               <div className="card">
--- src/page/detail.js
+++ src/page/detail.js
@@ -1,10 +1,29 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import blogs from "../data/blogs";
 
 export default function Detail() {
+  const [title, SetTitle] = useState("");
+  const [image, SetImage] = useState("");
+  const [content, SetContent] = useState("");
+  const [author, SetAuthor] = useState("");
   const { id } = useParams();
+  useEffect(() => {
+    const result = blogs.find((item) => item.id === parseInt(id));
+    SetTitle(result.title);
+    SetImage(result.image_url);
+    SetContent(result.content);
+    SetAuthor(result.author);
+    // eslint-disable-next-line
+  }, []);
   return (
-    <div>
-      <h2>รายระเอียดบทความ:{id}</h2>
+    <div className="container">
+      <h2 className="title">{title}</h2>
+      <img src={image} alt={title}></img>
+      <div className="blogs-deteil">
+        <strong>ผู้เขียน:{author}</strong>
+        <p>{content}</p>
+      </div>
     </div>
   );
 }
